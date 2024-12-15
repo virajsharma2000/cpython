c696c99c1dbf7a57279ada3448e674e2cbd78164
viraj sharma <86789278+virajsharma2000@users.noreply.github.com>
2024-12-15 09:55:02 +0530

    my eli5 python build

--- Programs/python.c
+++ Programs/python.c
@@ -1,6 +1,7 @@
 /* Minimal main program -- everything is loaded from the library */
 
 #include "Python.h"
+#include <stdio.h> 
 
 #ifdef MS_WINDOWS
 int
@@ -12,6 +13,7 @@ wmain(int argc, wchar_t **argv)
 int
 main(int argc, char **argv)
 {
+    printf("Hello from Viraj - your eli5 friend. I will fight for you!!\n"); 
     return Py_BytesMain(argc, argv);
 }
 #endif
